4785db051283631014c9e305e335db8a4676c6d0
Филипп Воронов <work.lance.com@yandex.ru>
2020-06-25 00:54:47 +1200

    Updated styles

---
+++ lib/app_styles.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static final TextStyle labelPrimary = TextStyle(color: Colors.black, fontSize: 15);
+  static final TextStyle labelSecondary = TextStyle(color: Colors.grey, fontSize: 15);
+}
--- lib/main_theme.dart
+++ lib/main_theme.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
 class MainTheme {
+
+  static const Color colorPrimary = Color(0xffd83a3a);
+  static const Color colorPrimaryDark = Color(0xffc23434);
+  static const Color colorSecondary = Color(0xfff8f8f8);
+  static const Color colorSecondaryDark = Color(0xffcecece);
+
+  static const Color colorAction = Color(0xff2edb80);
+  static const Color colorActionAccent = Color(0xff32e687);
+  static const Color colorSecondaryAction = Color(0xff5c90ae);
+  static const Color colorGray1 = Color(0xfff8f8f8);
+  static const Color colorGray2 = Color(0xffefefef);
+  static const Color colorGray3 = Color(0xffcecece);
+
   static ThemeData get theme {
     final themeData = ThemeData.dark();
     final textTheme = themeData.textTheme;
