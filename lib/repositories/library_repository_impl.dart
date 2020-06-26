a609af780487477cd0b8c655e120c5bc9ca0a93d
Филипп Воронов <work.lance.com@yandex.ru>
2020-06-26 15:14:26 +1200

    Updated library repository

--- lib/repositories/library_repository_impl.dart
+++ lib/repositories/library_repository_impl.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:musicboxflutter/models/library_item_model.dart';
-import 'package:musicboxflutter/repositories/library_repository.dart';
-import 'package:musicboxflutter/repositories/web_api.dart';
+import 'package:musicboxflutter/models/library_response_model.dart';
+import 'library_repository.dart';
+import 'web_api.dart';
 
 class LibraryRepositoryImpl implements LibraryRepository {
 
@@ -9,15 +12,18 @@ class LibraryRepositoryImpl implements LibraryRepository {
   LibraryRepositoryImpl({this.webApi});
 
   @override
-  Future<LibraryItemModel> findByIdAsync(String id) {
-    // TODO: implement findByIdAsync
-    throw UnimplementedError();
+  Future<LibraryItemModel> findByIdAsync(String id) async {
+    var library = await getAllAsync();
+    return library.firstWhere((element) => element.id == id,
+        orElse: () => null);
   }
 
   @override
-  Future<List<LibraryItemModel>> getAllAsync() {
-    // TODO: implement getAllAsync
-    throw UnimplementedError();
+  Future<List<LibraryItemModel>> getAllAsync() async {
+    var response = await webApi.requestLibraryJsonAsync();
+    var responseModel = LibraryResponseModel(json.decode(response));
+    responseModel?.throwIfHasErrors();
+    return responseModel?.data;
   }
 
 }
